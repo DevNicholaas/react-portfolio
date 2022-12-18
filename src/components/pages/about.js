343ddc136dc7cdd68b90ad6b538bea8b12c1706f
DevNicholaas <nicholaasmiller@gmail.com>
2022-12-18 14:01:06 -0800

    Updated syles in bio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -5,14 +5,14 @@ export default function() {
   return (
   <div className="content-page-wrapper">
     <div 
-    classname="left-column"
+    className="left-column"
     style={{
       background: "url(" + profilePicture + ") no-repeat",
       backdroundSize: "cover",
       backgroundPosition: "center"
       }}
     />
-    <div classname="right-column">
+    <div className="right-column">
       My name is Nicholaas Miller. I was born and raised in Oakland, California and I have worked very hard to be where I am. I found my interest in coding when I was in high school. Then when I was hired for my first job as a Recreation Aide for the City of Oakland I began my coding journey. For a short time I learned on my own then I signed up to go through Bottega University's Full-stack Development Program. During my time at Bottega I was prmoted to a Recreation Specialist and began teaching youth how to code. I also improved my own skills as well and built several applications. I worked closely with mentors and teachers to make sure I understood the complexities of everything I worked on.
     </div>
 
